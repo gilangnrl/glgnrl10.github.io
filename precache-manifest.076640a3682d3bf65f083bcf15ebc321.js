a5a8cb720395f989758172b8943ab496ac0e7625
Gilang Nuril Haqim <gilangn89@gmail.com>
2019-01-14 17:16:38 +0800

    Updates

---
+++ precache-manifest.076640a3682d3bf65f083bcf15ebc321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-hosting/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05e856938d9e65450d5c",
+    "url": "/test-hosting/static/js/runtime~main.05e85693.js"
+  },
+  {
+    "revision": "56610b91d85f5652092b",
+    "url": "/test-hosting/static/js/main.56610b91.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/test-hosting/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "56610b91d85f5652092b",
+    "url": "/test-hosting/static/css/main.1c20f109.chunk.css"
+  },
+  {
+    "revision": "359f56af5722cc0bb21b2c7355c147dc",
+    "url": "/test-hosting/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hello/precache-manifest.684fe800203eea839de4367af626ea9e.js"
+  "/test-hosting/precache-manifest.076640a3682d3bf65f083bcf15ebc321.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hello/index.html", {
+workbox.routing.registerNavigationRoute("/test-hosting/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
